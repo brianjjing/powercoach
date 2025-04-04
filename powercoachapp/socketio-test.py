aa8348a29c006220abc77ca6a7245f44d26eb529
heimlichmaneuve <brianjing1@gmail.com>
2025-04-04 14:34:22 -0700

    Resolved dependency issues

--- powercoachapp/socketio-test.py
+++ powercoachapp/socketio-test.py
@@ -1,17 +1,15 @@
-from urllib import response
 from flask_socketio import emit, SocketIOTestClient
 import requests
 from flask import request
 from powercoachapp.extensions import socketio
 from powercoachapp import create_app
 from powercoachapp.powercoachalgs import powercoachalg, active_clients
-from socketio.packet import Packet
 
 app = create_app()
 
 client = SocketIOTestClient(app, socketio)
 
-engine_url = 'ws://127.0.0.1:5000/socket.io/?EIO=4&transport=websocket'
+#engine_url = 'ws://127.0.0.1:5000/socket.io/?EIO=4&transport=websocket'
 
 client.connect()
 
