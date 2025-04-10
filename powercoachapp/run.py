9908ab8c232cbcc32d3d00682800ea20d961916f
heimlichmaneuve <brianjing1@gmail.com>
2025-04-10 01:16:50 -0700

    Changed frontend

--- powercoachapp/run.py
+++ powercoachapp/run.py
@@ -17,7 +17,7 @@ def log_request():
     for header, value in request.headers.items():
         print(f"{header}: {value}")
 
-port = int(os.environ.get("PORT", 10000))
+port = int(os.getenv("PORT", 10000))
 
 if __name__ == "__main__":
     socketio.run(app, host='0.0.0.0', port=port, debug=False, use_reloader=False)
