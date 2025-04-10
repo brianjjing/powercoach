6c91466edaea23ac30988dd113e5c05ada9ff3e6
heimlichmaneuve <brianjing1@gmail.com>
2025-04-09 19:01:51 -0700

    Trying different way of getting the port

--- powercoachapp/run.py
+++ powercoachapp/run.py
@@ -16,6 +16,7 @@ def log_request():
     for header, value in request.headers.items():
         print(f"{header}: {value}")
 
-port = int(os.environ.get("PORT", 10000))
+port = os.getenv('PORT', 10000)
+print(f"Using port: {port}")
 if __name__ == "__main__":
     socketio.run(app, host='0.0.0.0', port=port, debug=True)
