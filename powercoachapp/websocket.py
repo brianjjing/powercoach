47fb8d5f841042c861dd0232215493d094640ebc
heimlichmaneuve <brianjing1@gmail.com>
2025-06-30 16:42:23 -0700

    Added start powercoach and stop powercoach

--- powercoachapp/websocket.py
+++ powercoachapp/websocket.py
@@ -1,15 +1,14 @@
-import sys
-import os
+import sys, time
 import logging
 from flask_socketio import emit
 from flask import request
-from powercoachapp.extensions import socketio, logger
-from powercoachapp.OLDpowercoachalgs import powercoachalg, active_clients
+from powercoachapp.extensions import socketio, logger, clients
+from powercoachapp.powercoachalgmodern import powercoachalg, start_time
 
 @socketio.on('connect')
 def handle_connect():
     logger.info("Client connecting.")
-    active_clients.add(request.sid)
+    clients.add(request.sid)
     logger.info("Client added to active clients.")
     logger.info("Server is emitting connection event to client:")
     emit('connect_message', {'json_data': f'Client {request.sid} connected'})
@@ -17,8 +16,8 @@ def handle_connect():
 @socketio.on('disconnect')
 def handle_disconnect():
     logger.info("Client disconnecting.")
-    if request.sid in active_clients:
-        active_clients.remove(request.sid)
+    if request.sid in clients:
+        clients.remove(request.sid)
     logger.info("Client removed from active clients.")
     emit('disconnect_message', {'json_data': f'Client {request.sid} disconnected'})
     
@@ -29,7 +28,6 @@ def bruh():
     logging.info("TEST RESPONSE LOGGING")
     emit('test_response', {'status': 'received'})
 
-
 @socketio.on('test_message')
 def handle_test_message(message):
     sid = request.sid
@@ -37,13 +35,26 @@ def handle_test_message(message):
     logging.info("RECEIVED DA TEST MESSAGE, LOGGING")
     emit('test_response', {'status': 'received'})
 
+@socketio.on('start_powercoach')
+def start_powercoach():
+    global start_time
+    start_time = time.time()
+    logger.debug("Powercoach started")
+
 #PRINT AS LOGS (IMPORT LOGGING --> LOGGING.INFO("MESSAGE"))
-@socketio.on('handle_powercoach_frame') #will become handle_deadlift_frame
+@socketio.on('handle_powercoach_frame')
 def handle_powercoach_frame(base64_string):
     logger.info("POWERCOACH FRAME RECEIVED")
     logger.info(f"Length of base64_string[0]: {len(base64_string)}")
     logger.info(f"Byte size: {sys.getsizeof(base64_string)}")
+    
     powercoach_message = powercoachalg(base64_string)
     logger.info("Powercoach alg done on the frame")
-    emit('powercoach_message', [powercoach_message])
+    
+    emit('powercoach_message', powercoach_message)
     logger.info("Powercoach message emitted")
+    
+@socketio.on('stop_powercoach')
+def stop_powercoach():
+    start_time = None
+    logger.debug("Powercoach stopped")
